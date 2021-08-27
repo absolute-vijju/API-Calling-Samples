ab8a841cc01262ed2da7d8bbf54fef5db2d37f78
Vijay Koshti <53289959+absolute-vijju@users.noreply.github.com>
2021-08-27 15:16:45 +0530

    StateFlow-Retrofit-Hilt-Coroutine-MVVM

---
+++ StateFlow-Hilt-Retrofit/app/src/main/java/com/developer/vijay/stateflow/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.developer.vijay.stateflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.developer.vijay.stateflow.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "mydata"
+    }
+
+    private lateinit var mBinding: ActivityMainBinding
+    private val viewModel by viewModels<PostViewModel>()
+    private val postAdapter by lazy {
+        PostAdapter()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mBinding.title = "Post List"
+
+        mBinding.rvPosts.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = postAdapter
+        }
+
+        viewModel.getPosts()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.apiResponse.collect {
+                when (it) {
+                    is Resource.Error -> {
+                        mBinding.progressBar.visibility = View.GONE
+                        mBinding.rvPosts.visibility = View.GONE
+                        mBinding.title = it.message
+                        Log.d(TAG, "onCreate: Error")
+                    }
+                    is Resource.Loading -> {
+                        mBinding.progressBar.visibility = View.VISIBLE
+                        mBinding.rvPosts.visibility = View.GONE
+                        Log.d(TAG, "onCreate: Loading")
+                    }
+                    is Resource.Success -> {
+                        mBinding.progressBar.visibility = View.GONE
+                        mBinding.rvPosts.visibility = View.VISIBLE
+                        postAdapter.setData(it.data as PostResponse)
+                        Log.d(TAG, "onCreate: Success")
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ StateFlow-Hilt-Retrofit/app/src/main/java/com/developer/vijay/stateflow/PostRepo.kt
@@ -0,0 +1,12 @@
+package com.developer.vijay.stateflow
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class PostRepo @Inject constructor(private val api: API) {
+    fun callAPI() = flow {
+        emit(api.getPosts())
+    }.flowOn(Dispatchers.IO)
+}
